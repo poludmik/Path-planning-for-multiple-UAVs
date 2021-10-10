6828dce98b79ba8f59531fec7305333d52e2632b
poludmik <poludmik@fel.cvut.cz>
2021-10-10 22:15:21 +0200

    Code cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -208,8 +208,6 @@ int main(int argc, char **argv)
 	
 	while(ros::ok())
 	{
-		
-//		my_world.publish_world(vis_pub);
 		// get state
 		mrs_msgs::UavState::ConstPtr cur_uav_state;
 		if (ready)
@@ -220,8 +218,6 @@ int main(int argc, char **argv)
 		}
 		
 //		std::cout << cur_uav_state->pose.position.x << std::endl;
-//		std::cout << "publishing: [" << ros::Time::now().toSec() << "] " << cmd.reference.velocity.x << ", " << cmd.reference.velocity.y  << std::endl;
-//		vis_pub.publish(marker0);
 
 		ros::spinOnce();
 		rate.sleep();
@@ -229,78 +225,13 @@ int main(int argc, char **argv)
 	return EXIT_SUCCESS;
 }
 
+//mrs_msgs::VelocityReferenceStamped &cmd,
+//cmd.reference.velocity.y = 0;
+//vel_pub.publish(cmd);
+
 void odomCallback(mrs_msgs::UavState::ConstPtr const &msg)
 {
 	std::lock_guard<std::mutex> lock(uav_state_mutex);
 	uav_state = msg;
 	ready = true;
 }
-
-void setDefaultLineMarker(visualization_msgs::Marker& marker,
-                         float const x,
-                         float const y,
-                         float const z,
-                         float const size){
-	marker.header.frame_id = "map"; // "uav1/local_origin";
-	marker.header.stamp = ros::Time::now();
-	marker.ns = "basic_shapes";
-	marker.id = 0;
-	marker.type = visualization_msgs::Marker::SPHERE_LIST;
-	marker.action = visualization_msgs::Marker::ADD;
-	marker.pose.position.x = 1;
-	marker.pose.position.y = 0;
-	marker.pose.position.z = 1.2;
-	marker.pose.orientation.x = 0.0;
-	marker.pose.orientation.y = 0.0;
-	marker.pose.orientation.z = 0.0;
-	marker.pose.orientation.w = 1.0;
-	marker.scale.x = size;
-	marker.scale.y = size;
-	marker.scale.z = size;
-	marker.color.a = 0.3; // see-through or solid 1
-	marker.color.r = 1.0;
-	marker.color.g = 0.0;
-	marker.color.b = 0.0;
-	
-	marker.color.r = 1.0f;
-	marker.color.a = 1.0;
-	// Create the vertices for the points and lines
-	for (uint32_t i = 0; i < 10; ++i) {
-		float f = 0;
-		float g = 1;
-		
-		geometry_msgs::Point p;
-		p.x = (int32_t) i;
-		p.y = f;
-		p.z = g;
-		
-		marker.points.push_back(p);
-	}
-//	marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
-}
-
-void snakeMoves(mrs_msgs::VelocityReferenceStamped &cmd, std::vector<bool> &direction_state){
-	uint8_t counter = 41;
-	if (counter > 40) {
-		if (direction_state.at(0)) {
-			cmd.reference.velocity.x = 0.6;
-			cmd.reference.velocity.y = 0;
-		}
-		else if (direction_state.at(1) || direction_state.at(3)) {
-			cmd.reference.velocity.x = 0;
-			cmd.reference.velocity.y = 0.5;
-		}
-		else if (direction_state.at(2)) {
-			cmd.reference.velocity.x = -0.6;
-			cmd.reference.velocity.y = 0;
-		}
-		counter = 0;
-		short int i = 0;
-		std::rotate(direction_state.begin(), direction_state.begin()+1, direction_state.end());
-//			std::cout << "x = " << cmd.reference.velocity.x << ", y = " << cmd.reference.velocity.y << std::endl;
-	}
-	++counter;
-//	vel_pub.publish(cmd);
-};
-
-
